8d5fba04fe0f138cb60d760e359fa2feca05089d
Aanchal Kumari <126966402+Aanchal-kumari-R@users.noreply.github.com>
2023-03-07 03:36:42 +0530

    This is my First commit

--- app/src/main/java/com/example/myapplication2/MainActivity.kt
+++ app/src/main/java/com/example/myapplication2/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.myapplication2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,10 +18,10 @@ class MainActivity : AppCompatActivity() {
 
         val weightText = findViewById<EditText>(R.id.edit_1)
         val heightText = findViewById<EditText>(R.id.edit_2)
-        val calcButton = findViewById<Button>(R.id.button_1)
-        val bmivalue = findViewById<TextView>(R.id.textveiw_1)
+        val calcButton = findViewById<Button>(R.id.edit_3)
+        val bmivalue = findViewById<TextView>(R.id.edit_4)
 
-        val status = findViewById<TextView>(R.id.textview2)
+        val status = findViewById<TextView>(R.id.edit_5)
         var isclear=false
 
 
@@ -42,7 +45,10 @@ class MainActivity : AppCompatActivity() {
 
                     var wi = weightText.text.toString().toDouble()
                     var hi = heightText.text.toString().toDouble()
-                    val bmi = wi / (hi / 100) * (hi / 100)
+                    val total = wi / (hi / 100) * (hi / 100)
+                    val bmi=(total*100).roundToInt()/100.0
+
+
                     bmivalue.text = "Your BMI value=" + bmi.toString()
                     if (bmi > 25) {
                         Toast.makeText(this, "you are overweigth", Toast.LENGTH_LONG).show()
@@ -56,10 +62,12 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
                 else if(isclear){
+                    calcButton.text="Calculate BMI"
                     weightText.text.clear()
                     heightText.text.clear()
                     bmivalue.text=""
                     status.text=""
+                    isclear=false
 
                 }
             } else {
@@ -67,19 +75,38 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "please enter the value", Toast.LENGTH_LONG).show()
             }
 
-        }
+    }
     }
 
-}
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater=menuInflater
+        menuInflater.inflate(R.menu.menu_item,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.about_bmi -> {
+                Toast.makeText(this, "About BMI", Toast.LENGTH_LONG).show()
+                return true
+            }
+            R.id.bmi_chart -> {
+                Toast.makeText(this, "BMI chart", Toast.LENGTH_LONG).show()
+                return true
+            }
+            R.id.exit_app -> {
+                Toast.makeText(this, "Exit", Toast.LENGTH_LONG).show()
+                finish()
+                System.exit(0)
+                return true
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+    }
+        override fun onBackPressed() {
+             Toast.makeText(this,"back button not work",Toast.LENGTH_LONG).show();
+         }
 
 
-
-
-
-
-
-
-
-
-
-
+    }
